3376a0f67cc4473f27e6fc5dd45b6178457f08d4
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-06-24 15:24:36 +0900

    Chore: 이미지 추가

--- NineNine/NineNine/Model/MainData.swift
+++ NineNine/NineNine/Model/MainData.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct GameData {
+struct MainData {
     private let gameTitles: [String] = ["탭탭!", "쉐킷쉐킷!", "부비부비"]
     private var gameImages: [UIImage] = [#imageLiteral(resourceName: "tabtab"), #imageLiteral(resourceName: "Shake"), #imageLiteral(resourceName: "swipe")]
     private var swipingCatImages:[UIImage] = [#imageLiteral(resourceName: "left"), #imageLiteral(resourceName: "right")]
